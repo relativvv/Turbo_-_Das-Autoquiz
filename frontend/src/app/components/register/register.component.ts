54165caba8919bec25ecddd3b4575fa02320aa6d
Robin Schulte <r.schulte@shopware.com>
2022-09-01 13:36:07 +0200

    add title to components

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly titleService: Title
+  ) {
+    this.titleService.setTitle('Turbo - Das Autoquiz | Registrieren');
+  }
 
   ngOnInit(): void {
   }
--- frontend/src/app/components/specify-mode/specify-mode.component.ts
+++ frontend/src/app/components/specify-mode/specify-mode.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-specify-mode',
@@ -11,8 +12,11 @@ export class SpecifyModeComponent implements OnInit {
   mode: 'difficulty' | 'category';
 
   constructor(
-    private route: ActivatedRoute
-  ) { }
+    private readonly route: ActivatedRoute,
+    private readonly titleService: Title
+  ) {
+    this.titleService.setTitle('Turbo - Das Autoquiz | Modus auswählen');
+  }
 
   ngOnInit(): void {
     this.getMode();
