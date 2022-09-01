e50d74755dcd1353e35cdea2946b9b782b8c0b88
Robin Schulte <r.schulte@shopware.com>
2022-09-01 09:07:25 +0200

    add login and register page

--- frontend/src/app/shared/profile/profile.component.ts
+++ frontend/src/app/shared/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly userService: UserService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
 
 }
