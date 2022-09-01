949f79ad4d7ce846ef42d963272a7a91d7006fe6
Robin Schulte <r.schulte@shopware.com>
2022-09-01 13:44:32 +0200

    add quiz basics

---
+++ frontend/src/app/components/quiz/quiz.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizComponent } from './quiz.component';
+
+
+
+@NgModule({
+  declarations: [
+    QuizComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuizModule { }
