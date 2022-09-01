d951ef9c07607901601b7f6faa96beb596afe6f1
Robin Schulte <r.schulte@shopware.com>
2022-09-01 13:57:17 +0200

    add angular animations

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.less']
+  styleUrls: ['./home.component.less'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ position: 'relative', top: 25, opacity: 0 }),
+        animate('1000ms', style({ opacity: 1, top: 0 }))
+      ]),
+      transition(':leave', [
+        style({ top: 0, opacity: 1 }),
+        animate('1000ms', style({ opacity: 0, top: 100 }))
+      ]),
+    ])
+  ],
 })
 export class HomeComponent implements OnInit {
 
--- frontend/src/app/components/specify-mode/specify-mode.component.ts
+++ frontend/src/app/components/specify-mode/specify-mode.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Title} from "@angular/platform-browser";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-specify-mode',
   templateUrl: './specify-mode.component.html',
-  styleUrls: ['./specify-mode.component.less']
+  styleUrls: ['./specify-mode.component.less'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ position: 'relative', top: 35, opacity: 0 }),
+        animate('1000ms', style({ opacity: 1, top: 0 }))
+      ]),
+      transition(':leave', [
+        style({ top: 0, opacity: 1 }),
+        animate('1000ms', style({ opacity: 0, top: 100 }))
+      ]),
+    ])
+  ],
 })
 export class SpecifyModeComponent implements OnInit {
 
